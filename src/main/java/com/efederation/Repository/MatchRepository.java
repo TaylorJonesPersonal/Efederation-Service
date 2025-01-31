4c01f1f22ff4e062b440c75a726b077db245b832
TaylorJonesPersonal <jonesftwingp@outlook.com>
2025-01-30 19:05:13 -0800

    Order matches by date descending

--- src/main/java/com/efederation/Repository/MatchRepository.java
+++ src/main/java/com/efederation/Repository/MatchRepository.java
@@ -12,7 +12,8 @@ public interface MatchRepository extends JpaRepository<Match, String> {
     @Query(value = "select match_id, winner, created_at, npc_participants_npc_id, condition" +
             " from match m inner join match_human_participants mhp on m.match_id = mhp.match_match_id" +
             " inner join match_npc_participants mnp on m.match_id = mnp.match_match_id" +
-            " WHERE mhp.human_participants_wrestler_id = :id", nativeQuery = true)
+            " WHERE mhp.human_participants_wrestler_id = :id" +
+            " ORDER BY created_at DESC", nativeQuery = true)
     List<Map<String, Object>> getMatchesByWrestlerId(@Param("id") int id);
 
 }
