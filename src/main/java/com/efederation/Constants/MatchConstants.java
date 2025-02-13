d2354c071be1a24e84f33cd8e534046dd65a9ccb
TaylorJonesPersonal <jonesftwingp@outlook.com>
2025-02-12 21:39:44 -0800

    Added more events

--- src/main/java/com/efederation/Constants/MatchConstants.java
+++ src/main/java/com/efederation/Constants/MatchConstants.java
@@ -9,22 +9,27 @@ import java.util.*;
 public class MatchConstants {
     private final String[] moves = {
             "Snap Suplex", "Snapmare", "Corner Running Splash", "Dropkick", "Running Lariat",
-    "Powerbomb", "Scoop Slam", "Running Knee"
+    "Powerbomb", "Scoop Slam", "Running Knee", "DDT", "Back Body Drop"
     };
 
     private final String DEVASTATION = "Devastation|%WINNER performed a %MOVE. %WINNER then performed a %MOVE on %LOSER. After a long battle, %WINNER wins by %VICTORY_CONDITION.";
+    private final String LADDER_ATTACK = "Ladder Attack|%WINNER hit %LOSER with a ladder!";
+    private final String STEEL_CHAIR_MISFIRE = "Steel Chair Misfire|%LOSER tried to hit %WINNER with a chair, but missed!";
 
     // make a custom class here with private var String[] for each %s and what it represents. This can be parsed and
     // winners, losers and etc. can be understood to be passed here.
-    private final List<String> matchEventArr = List.of(DEVASTATION);
+    private final List<String> matchEventArr = List.of(DEVASTATION, LADDER_ATTACK);
 
     public Set<String> drawEvents() {
         Set<String> drawnEvents = new HashSet<>();
         Random random = new Random();
-        int roll = random.nextInt(matchEventArr.size());
-        for(String event : matchEventArr) {
-            if(matchEventArr.indexOf(event) == roll) {
-                drawnEvents.add(event);
+        int numberOfEvents = random.nextInt(5);
+        for(int i = 0; i < numberOfEvents; i++) {
+            int roll = random.nextInt(matchEventArr.size());
+            for(String event : matchEventArr) {
+                if(matchEventArr.indexOf(event) == roll) {
+                    drawnEvents.add(event);
+                }
             }
         }
         return drawnEvents;
